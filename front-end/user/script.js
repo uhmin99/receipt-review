2baf6235fbacd6c01c3c7104b58730c25eb5cc5f
유현민 <sayiloveyou8622@gmail.com>
2023-07-14 15:50:52 +0900

    feat: basic web ui for starting data transmission
    - at this moment planning to just moving data in the data folder to lakehouse

---
+++ front-end/user/script.js
@@ -0,0 +1,12 @@
+// Add an event listener to the start button
+document.getElementById('start-button').addEventListener('click', handleStartTransmission);
+
+
+// Function to handle the button click event
+function handleStartTransmission() {
+    alert('Data transmission started!');
+
+    //find data folder
+
+    //send data to lakehouse
+}
