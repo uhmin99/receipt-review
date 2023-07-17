aaaebc00f1e5d0cab42c2a99975a80c9e3d429c2
유현민 <sayiloveyou8622@gmail.com>
2023-07-17 16:31:12 +0900

    chore: simple script for reseting delta lake folder
    - doesn't support window

---
+++ lakehouse/script/clear_lakehouse.sh
@@ -0,0 +1,12 @@
+cd ../storage
+rm -rf receipt-image-delta
+rm -rf receipt-json-delta
+
+mkdir receipt-image-delta
+cd receipt-image-delta
+touch .gitkeep
+
+cd ../
+mkdir receipt-json-delta
+cd receipt-json-delta
+touch .gitkeep
